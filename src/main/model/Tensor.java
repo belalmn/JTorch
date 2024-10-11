25dd7d1fa07e0ca9b5fd09147e9d17769a11e6d5
Belal Mohammed-Nur <belal.mnur@gmail.com>
2024-10-11 16:47:15 -0700

    Implemented Tensor

--- src/main/model/Tensor.java
+++ src/main/model/Tensor.java
@@ -8,25 +8,64 @@ public class Tensor {
     // EFFECTS: initializes this tensor with the given data;
     // throws IllegalArgumentException if data is null
     public Tensor(double[][] data) {
-        // TODO: implement constructor
+        if (data == null) {
+            throw new IllegalArgumentException("Data cannot be null");
+        }
+        int rows = data.length;
+        int cols = data[0].length;
+        this.data = new double[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            if (data[i].length != cols) {
+                throw new IllegalArgumentException("All rows must have the same number of columns");
+            }
+            System.arraycopy(data[i], 0, this.data[i], 0, cols); // Faster copying
+        }
     }
 
     // MODIFIES: this
     // EFFECTS: adds the elements of other to the elements of this tensor;
     // throws IllegalArgumentException if other is null or dimensions do not match
     public void add(Tensor other) {
-        // TODO: implement add
+        if (other == null) {
+            throw new IllegalArgumentException("Other tensor cannot be null");
+        }
+        double[][] otherData = other.getData();
+        if (data.length != otherData.length || data[0].length != otherData[0].length) {
+            throw new IllegalArgumentException("Tensor dimensions must match");
+        }
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                data[i][j] += otherData[i][j];
+            }
+        }
     }
 
     // MODIFIES: this
     // EFFECTS: multiplies the elements of this tensor with the elements of the other tensor;
     // throws IllegalArgumentException if other is null or dimensions do not match
     public void multiply(Tensor other) {
-        // TODO: implement multiply
+        if (other == null) {
+            throw new IllegalArgumentException("Other tensor cannot be null");
+        }
+        double[][] otherData = other.getData();
+        if (data.length != otherData.length || data[0].length != otherData[0].length) {
+            throw new IllegalArgumentException("Tensor dimensions must match");
+        }
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                data[i][j] *= otherData[i][j];
+            }
+        }
     }
 
     // EFFECTS: returns the data of this tensor
     public double[][] getData() {
-        return null; // stub
+        int rows = data.length;
+        int cols = data[0].length;
+        double[][] copyData = new double[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            System.arraycopy(data[i], 0, copyData[i], 0, cols);
+        }
+        return copyData;
     }
 }
