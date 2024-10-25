5ce552a46b921db7db13ef897fb58adfec4db2ec
Belal Mohammed-Nur <belal.mnur@gmail.com>
2024-10-25 16:52:35 -0700

    Implemented JsonReader and JsonWriter

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -3,25 +3,31 @@ package persistence;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 // Represents a reader that reads neural network from JSON data stored in file
 public class JsonReader {
+    private String source;
 
     // MODIFIES: this
     // EFFECTS: Constructs a reader to read from source file
     public JsonReader(String source) {
-        
+        this.source = source;
     }
 
     // MODIFIES: this
     // EFFECTS: Reads JSON from file and returns it
     public JSONObject readJson() throws IOException {
-        return null;
+        String jsonData = readFile(source);
+        return new JSONObject(jsonData);
     }
 
     // MODIFIES: this
     // EFFECTS: Reads source file as string and returns it
     private String readFile(String source) throws IOException {
-        return null;
+        byte[] encoded = Files.readAllBytes(Paths.get(source));
+        return new String(encoded, StandardCharsets.UTF_8);
     }
 }
--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -8,31 +8,34 @@ import java.io.*;
 
 // Represents a reader that reads neural network from JSON data on file
 public class JsonWriter {
+    private PrintWriter writer;
+    private String destination;
 
     // EFFECTS: Constructs a writer to write to destination file
     public JsonWriter(String destination) {
-        
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: Opens the writer
     public void open() throws FileNotFoundException {
-
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: nn
     // EFFECTS: Writes JSON representation of a Writable object to file
     public void write(Writable writable) {
-
+        JSONObject json = writable.toJson();
+        saveToFile(json.toString(4)); // Indent with 4 spaces
     }
 
     // EFFECTS: Closes the writer
     public void close() {
-
+        writer.close();
     }
 
     // EFFECTS: Writes string to file
     private void saveToFile(String json) {
-
+        writer.print(json);
     }
 }
