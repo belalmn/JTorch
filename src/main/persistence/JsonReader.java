dd7782f52c5b7e8baef944e41893bc185b2c4b0d
Belal Mohammed-Nur <belal.mnur@gmail.com>
2024-10-25 16:50:32 -0700

    Added specifications for JsonReader

---
+++ src/main/persistence/JsonReader.java
@@ -0,0 +1,27 @@
+package persistence;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+// Represents a reader that reads neural network from JSON data stored in file
+public class JsonReader {
+
+    // MODIFIES: this
+    // EFFECTS: Constructs a reader to read from source file
+    public JsonReader(String source) {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Reads JSON from file and returns it
+    public JSONObject readJson() throws IOException {
+        return null;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Reads source file as string and returns it
+    private String readFile(String source) throws IOException {
+        return null;
+    }
+}
