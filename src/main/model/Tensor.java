455fe60b6cae5e59fada8eb1cecef72b7bd87ba1
Belal Mohammed-Nur <belal.mnur@gmail.com>
2024-11-28 22:39:39 -0800

    Added event logging to Tensor class

--- src/main/model/Tensor.java
+++ src/main/model/Tensor.java
@@ -32,10 +32,13 @@ public class Tensor implements Writable {
     // throws IllegalArgumentException if other is null or dimensions do not match
     public void add(Tensor other) {
         if (other == null) {
+            EventLog.getInstance().logEvent(new Event("Attempted to add a null tensor."));
             throw new IllegalArgumentException("Other tensor cannot be null");
         }
         double[][] otherData = other.getData();
         if (data.length != otherData.length || data[0].length != otherData[0].length) {
+            EventLog.getInstance().logEvent(new Event("Tensor dimension mismatch in add operation: dimensions "
+                    + data.length + "x" + data[0].length + " and " + otherData.length + "x" + otherData[0].length));
             throw new IllegalArgumentException("Tensor dimensions must match");
         }
         for (int i = 0; i < data.length; i++) {
@@ -50,10 +53,13 @@ public class Tensor implements Writable {
     // throws IllegalArgumentException if other is null or dimensions do not match
     public void multiply(Tensor other) {
         if (other == null) {
+            EventLog.getInstance().logEvent(new Event("Attempted to multiply by a null tensor."));
             throw new IllegalArgumentException("Other tensor cannot be null");
         }
         double[][] otherData = other.getData();
         if (data.length != otherData.length || data[0].length != otherData[0].length) {
+            EventLog.getInstance().logEvent(new Event("Tensor dimension mismatch in multiply operation: dimensions "
+                    + data.length + "x" + data[0].length + " and " + otherData.length + "x" + otherData[0].length));
             throw new IllegalArgumentException("Tensor dimensions must match");
         }
         for (int i = 0; i < data.length; i++) {
@@ -86,6 +92,8 @@ public class Tensor implements Writable {
             dataArray.put(rowArray);
         }
         json.put("data", dataArray);
+        EventLog.getInstance().logEvent(new Event("Serialized Tensor to JSON with dimensions "
+                + data.length + "x" + data[0].length));
         return json;
     }
 
@@ -101,6 +109,8 @@ public class Tensor implements Writable {
                 data[i][j] = rowArray.getDouble(j);
             }
         }
+        EventLog.getInstance().logEvent(new Event("Deserialized Tensor from JSON with dimensions "
+                + data.length + "x" + data[0].length));
         return new Tensor(data);
     }
 }
